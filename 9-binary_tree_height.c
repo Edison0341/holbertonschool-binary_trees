d44c565c88f47b1564c24243a759eb8abfe4f121
edison0341 <edison0341@gmail.com>
2024-07-30 17:50:58 -0400

    checkpoint

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,7 +4,7 @@
  * @tree: pointer to the root node of the tree to measure the height
  * Return: hieght of the binary tree in number
  */
-size_t binary_tree_height(const binary_tree_t tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
