83c579b3b7af595ce147fc54ca7543120287643f
edison0341 <edison0341@gmail.com>
2024-07-30 17:57:32 -0400

    function that checks for depth of a binary tree

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - check for depth of a binary tree
+ * @tree: pointer to the node to measure the depth
+ * Return: depth of a node in a binary tree
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->parent == NULL)
+		return (0);
+	return (binary_tree_depth(tree->parent) + 1);
+}
