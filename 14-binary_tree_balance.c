1523b22eee751ac583c86d55bea9dd11dbeef9e1
edison0341 <edison0341@gmail.com>
2024-08-01 14:42:30 -0400

    adding two projects with fixed betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -23,18 +23,15 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_height, right_height;
+	size_t left_height, right_height;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        left_height = binary_tree_height(tree->left);
-        right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-        if (tree->right == NULL && tree->left == NULL)
-                return (0);
-
-        if (right_height > left_height)
-                return (right_height + 1);
-        return (left_height + 1);
+	if (right_height > left_height)
+		return (right_height + 1);
+	return (left_height + 1);
 }
